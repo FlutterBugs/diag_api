1d81a84aa8f81fb251e32b365e1be71f6e77e0de
FlutterBugs <68394657+FlutterBugs@users.noreply.github.com>
2022-06-22 08:34:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import pickle
 import joblib
 import numpy as np
+import pandas as pd
 import traceback
 
 app = Flask(__name__)
@@ -15,13 +16,42 @@ def predict():
     lr = joblib.load("diagnosis_model.pkl")
     if lr:
         try:
-            json = request.get_json()  
-            model_columns = joblib.load("diagnosis_cols.pkl")
-            temp=list(json[0].values())
-            vals=np.array(temp)
-            prediction = lr.predict(temp)
-            print("here:",prediction)        
-            return jsonify({'prediction': str(prediction[0])})
+            radius_mean = request.form['radius_mean']
+            texture_mean = request.form['texture_mean']
+            perimeter_mean = request.form['perimeter_mean']
+            area_mean = request.form['area_mean']
+            smoothness_mean = request.form['smoothness_mean']
+            compactness_mean = request.form['compactness_mean']
+            concavity_mean = request.form['concavity_mean']
+            concave_points_mean = request.form['concave points_mean']
+            symmetry_mean = request.form['symmetry_mean']
+            fractal_dimension_mean = request.form['fractal_dimension_mean']
+            radius_se = request.form['radius_se']
+            texture_se = request.form['texture_se']
+            perimeter_se = request.form['perimeter_se']
+            area_se = request.form['area_se']
+            smoothness_se = request.form['smoothness_se']
+            compactness_se = request.form['compactness_se']
+            concavity_se = request.form['concavity_se']
+            concave_points_se = request.form['concave points_se']
+            symmetry_se = request.form['symmetry_se']
+            fractal_dimension_se = request.form['fractal_dimension_se']
+            radius_worst = request.form['radius_worst']
+            texture_worst = request.form['texture_worst']
+            perimeter_worst = request.form['perimeter_worst']
+            area_worst = request.form['area_worst']
+            smoothness_worst = request.form['smoothness_worst']
+            compactness_worst = request.form['compactness_worst']
+            concavity_worst = request.form['concavity_worst']
+            concave_points_worst = request.form['concave points_worst']
+            symmetry_worst = request.form['symmetry_worst']
+            fractal_dimension_worst = request.form['fractal_dimension_worst']
+            
+            data = [[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave points_worst, symmetry_worst, fractal_dimension_worst]]
+            prediction_data = pd.DataFrame(data, columns=['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst', 'fractal_dimension_worst'])
+            prediction = lr.predict(prediction_data)
+            
+            return jsonify(prediction[0])
         except:        
             return jsonify({'trace': traceback.format_exc()})
     else:
